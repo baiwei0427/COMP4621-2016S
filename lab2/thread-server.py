9119a5c7a796c5b0b53aede8945de22fe18dbe3a
wei bai <baiwei0427@sina.com>
2016-02-28 20:57:13 +0800

    Remove lock from thread-server.py

--- lab2/thread-server.py
+++ lab2/thread-server.py
@@ -2,10 +2,9 @@ import socket
 import threading
 
 class ServerThread(threading.Thread):
-	def __init__(self, client_socket, addr, lock):
+	def __init__(self, client_socket, addr):
 		self.socket = client_socket
 		self.addr = addr 
-		self.lock = lock
 		threading.Thread.__init__(self)
 	
 	def run(self):
@@ -15,11 +14,8 @@ class ServerThread(threading.Thread):
 		self.socket.send(line[::-1])
 		# close the connection
 		self.socket.close()
-		self.lock.acquire()
 		# print information
 		print "Receive %s from %s" % (line, str(self.addr))
-		self.lock.release()
-	
 	
 # create a socket object using TCP (SOCK_STREAM) as the transport protocol
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -33,12 +29,10 @@ server_port = 12345
 s.bind((server_ip, server_port))
 # queue up to 5 requests
 s.listen(5)
-# We need a lock for multiple threads 
-lock = threading.Lock()
 
 while True:
 	# establish a connection
 	client_socket, addr = s.accept()
-	t = ServerThread(client_socket, addr, lock)
+	t = ServerThread(client_socket, addr)
 	t.start() 
 
